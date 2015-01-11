644ff871abd478307af73b2754f53080262c3ec8
Coen Jacobs <coenjacobs@gmail.com>
2015-01-11 15:31:57 +0100

    Mock WordPress functions in spec

--- spec/WPUpdatePhpSpec.php
+++ spec/WPUpdatePhpSpec.php
@@ -1,24 +1,31 @@
 <?php
 
-namespace spec;
-
-use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
-
-class WPUpdatePhpSpec extends ObjectBehavior
-{
-    function let()
-    {
-        $this->beConstructedWith('5.4.0');
+namespace {
+    function add_action( $hook, $callback ) {
+        return true;
     }
 
-    function it_can_run_on_minimum_version()
-    {
-        $this->does_it_meet_required_php_version('5.4.0')->shouldReturn(true);
+    function is_admin() {
+        return true;
     }
+}
 
-    function it_will_not_run_on_old_version()
-    {
-        $this->does_it_meet_required_php_version('5.2.4')->shouldReturn(false);
+namespace spec {
+
+    use PhpSpec\ObjectBehavior;
+    use Prophecy\Argument;
+
+    class WPUpdatePhpSpec extends ObjectBehavior {
+        function let() {
+            $this->beConstructedWith( '5.4.0' );
+        }
+
+        function it_can_run_on_minimum_version() {
+            $this->does_it_meet_required_php_version( '5.4.0' )->shouldReturn( true );
+        }
+
+        function it_will_not_run_on_old_version() {
+            $this->does_it_meet_required_php_version( '5.2.4' )->shouldReturn( false );
+        }
     }
 }
