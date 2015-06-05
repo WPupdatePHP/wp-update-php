87d7798a939a537e49265758eebb10195742c562
Coen Jacobs <coenjacobs@gmail.com>
2015-06-05 09:38:21 +0200

    Separate method for getting the admin notice to split it up

--- src/WPUpdatePhp.php
+++ src/WPUpdatePhp.php
@@ -80,18 +80,36 @@ class WPUpdatePhp {
 	}
 
 	/**
+	 * Returns the string to be shown in the admin notice based on the level ('recommended' or default 'minimum')
+	 * of the notice. This will also add the plugin name to the notice string, if set.
+	 *
+	 * @param $level String ('recommended' or 'minimum' as default)
+	 *
+	 * @return string
+	 */
+	public function get_admin_notice( $level = 'minimum' ) {
+		if ( 'recommended' == $level ) {
+			if ( ! empty( $this->plugin_name ) ) {
+				return '<p>'. $this->plugin_name .' recommends a PHP versions higher than '. $this->recommended_version .'. Read more information about <a href="http://www.wpupdatephp.com/update/">how you can update</a>.</p>';
+			} else {
+				return '<p>This plugin recommends a PHP versions higher than '. $this->recommended_version .'. Read more information about <a href="http://www.wpupdatephp.com/update/">how you can update</a>.</p>';
+			}
+		}
+
+		if ( ! empty( $this->plugin_name ) ) {
+			return '<p>Unfortunately, '. $this->plugin_name .' can not run on PHP versions older than '. $this->minimum_version .'. Read more information about <a href="http://www.wpupdatephp.com/update/">how you can update</a>.</p>';
+		} else {
+			return '<p>Unfortunately, this plugin can not run on PHP versions older than '. $this->minimum_version .'. Read more information about <a href="http://www.wpupdatephp.com/update/">how you can update</a>.</p>';
+		}
+	}
+
+	/**
 	 * Method hooked into admin_notices when minimum PHP version is not available to show this in a notice
 	 * @hook admin_notices
 	 */
 	public function minimum_admin_notice() {
 		echo '<div class="error">';
-
-		if ( ! empty( $this->plugin_name ) ) {
-			echo '<p>Unfortunately, '. $this->plugin_name .' can not run on PHP versions older than '. $this->minimum_version .'. Read more information about <a href="http://www.wpupdatephp.com/update/">how you can update</a>.</p>';
-		} else {
-			echo '<p>Unfortunately, this plugin can not run on PHP versions older than '. $this->minimum_version .'. Read more information about <a href="http://www.wpupdatephp.com/update/">how you can update</a>.</p>';
-		}
-
+		echo $this->get_admin_notice( 'minimum' );
 		echo '</div>';
 	}
 
@@ -101,13 +119,7 @@ class WPUpdatePhp {
 	 */
 	public function recommended_admin_notice() {
 		echo '<div class="error">';
-
-		if ( ! empty( $this->plugin_name ) ) {
-			echo '<p>'. $this->plugin_name .' recommends a PHP versions higher than '. $this->recommended_version .'. Read more information about <a href="http://www.wpupdatephp.com/update/">how you can update</a>.</p>';	
-		} else {
-			echo '<p>This plugin recommends a PHP versions higher than '. $this->recommended_version .'. Read more information about <a href="http://www.wpupdatephp.com/update/">how you can update</a>.</p>';	
-		}
-		
+		echo $this->get_admin_notice( 'recommended' );
 		echo '</div>';
 	}
 }
