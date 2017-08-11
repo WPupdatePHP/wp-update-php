7cd9152119872bc4b69e98cc8b8620b2f4be3393
Coen Jacobs <coenjacobs@gmail.com>
2017-08-11 15:01:53 +0200

    Fix indenting

--- spec/WPUP/TranslatorSpec.php
+++ spec/WPUP/TranslatorSpec.php
@@ -9,13 +9,12 @@ class WPUP_TranslatorSpec extends ObjectBehavior
 {
 	function it_should_return_new_set_string()
 	{
-			$this->setString('minimum', 'test string');
-			$this->getString('minimum')->shouldMatch('/test string/i');
+        $this->setString('minimum', 'test string');
+        $this->getString('minimum')->shouldMatch('/test string/i');
 	}
-
 
 	function it_returns_false_if_no_string_is_set_for_requested_key()
 	{
-			$this->getString('test')->shouldReturn(false);
+        $this->getString('test')->shouldReturn(false);
 	}
 }
